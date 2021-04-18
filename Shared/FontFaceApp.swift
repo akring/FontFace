e25737a63e8ed5a918a9d7280b67b38cec624861
Akring <ddflj3210@gmail.com>
2021-04-18 20:34:12 +0800

    Initial Commit

---
+++ Shared/FontFaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontFaceApp.swift
+//  Shared
+//
+//  Created by 吕俊 on 2021/4/18.
+//
+
+import SwiftUI
+
+@main
+struct FontFaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
